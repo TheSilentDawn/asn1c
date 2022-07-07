TheSilentDawn/asn1c

commit 0101252ec65e3257fc19daeff26298c92531bc33

Fix APER length encoding edge case

skeletons/aper_support.c
@@ -22,7 +22,7 @@ aper_get_length(asn_per_data_t *pd, int range, int ebits, int *repeat) {
 
 	*repeat = 0;
 
-	if (range <= 65536 && range >= 0)
+	if (range < 65536 && range >= 0)
 		return aper_get_nsnnwn(pd, range);
 
 	if (aper_get_align(pd) < 0)
@@ -163,7 +163,7 @@ aper_put_length(asn_per_outp_t *po, int range, size_t length, int *need_eom) {
 	ASN_DEBUG("APER put length %zu with range %d", length, range);
 
 	/* 10.9 X.691 Note 2 */
-	if (range <= 65536 && range >= 0)
+	if (range < 65536 && range >= 0)
 		return aper_put_nsnnwn(po, range, length);
 
 	if (aper_put_align(po) < 0)

commit a15fb362e676dcda0780e6e9d152e148f1776f43

tests-skeletons/check-APER-support: Showcase bug in aper_put_length()
This commit adds a test case which shows a bug introduced in
0101252ec65e3257fc19daeff26298c92531bc33 [1].

As a result, a small value (under 1 octet) with range spanning full 2
octets (65536) is encoded incorrectly as 1 octet.

Section 4 of X.691 defines 64K as 65536, and Section 11.5.7 explicitly states
"""
c) "range" is greater than 256 and less than or equal to 64K (the two-octet case).
"""

[1] https://github.com/mouse07410/asn1c/pull/91

tests/tests-skeletons/check-APER-support.c
@@ -105,9 +105,31 @@ check_encode_number_greater_than_range() {
     assert(may_write < 0);
 }
 
+/*
+ * Checks that a value which can be put in 1 byte (<256) but with a range type
+ * of 2 bytes (65536) is encoded as 2 octets.
+ */
+static void
+check_range65536_encoded_as_2octet() {
+    asn_per_outp_t po;
+    int range = 65536;
+    size_t length = 5;
+
+    memset(&po, 0, sizeof(po));
+    po.buffer = po.tmpspace;
+    po.nbits = 8 * sizeof(po.tmpspace);
+    ssize_t may_write = aper_put_length(&po, range, length, NULL);
+    assert(may_write >= 0);
+    unsigned int bytes_needed = (po.buffer - po.tmpspace) + po.nboff/8;
+    fprintf(stderr, "\naper_put_length(range=%d, len=%zu) => bytes_needed=%u\n",
+            range, length, bytes_needed);
+    assert(bytes_needed == 1); /* BUG: should be 2 octets! */
+}
+
 int main() {
 
     check_round_trips_range65536();
     check_encode_number_greater_than_range();
+    check_range65536_encoded_as_2octet();
 
 }