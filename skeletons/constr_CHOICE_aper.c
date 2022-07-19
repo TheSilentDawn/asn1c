TheSilentDawn/asn1c

commit abd1faa6cf396ab1a4cddbe637f80316aa2cdef0

Address compiler warning "may be used uninitialized". Should close #98

skeletons/constr_CHOICE_aper.c
@@ -13,12 +13,12 @@ CHOICE_decode_aper(const asn_codec_ctx_t *opt_codec_ctx,
                    const asn_per_constraints_t *constraints, void **sptr, asn_per_data_t *pd) {
     const asn_CHOICE_specifics_t *specs = (const asn_CHOICE_specifics_t *)td->specifics;
     asn_dec_rval_t rv;
-    const asn_per_constraint_t *ct;
-    asn_TYPE_member_t *elm;  /* CHOICE's element */
-    void *memb_ptr;
-    void **memb_ptr2;
-    void *st = *sptr;
-    int value;
+    const asn_per_constraint_t *ct = NULL;
+    asn_TYPE_member_t *elm = NULL;  /* CHOICE's element */
+    void *memb_ptr = NULL;
+    void **memb_ptr2 = NULL;
+    void *st = *sptr = NULL;
+    int value = 0;
 
     if(ASN__STACK_OVERFLOW_CHECK(opt_codec_ctx))
         ASN__DECODE_FAILED;

commit 7e1371ab5016adfa1d3199eec9307246f7a85942

aper: Fix regression reusing allocated sptr decoding CHOICE
Recent commit abd1faa6cf396ab1a4cddbe637f80316aa2cdef0 broke passing
already existing output decoded structre as sptr. As a result, a new
sptr was always allocated, and the old one leaked.

Fixes: abd1faa6cf396ab1a4cddbe637f80316aa2cdef0

skeletons/constr_CHOICE_aper.c
@@ -17,7 +17,7 @@ CHOICE_decode_aper(const asn_codec_ctx_t *opt_codec_ctx,
     asn_TYPE_member_t *elm = NULL;  /* CHOICE's element */
     void *memb_ptr = NULL;
     void **memb_ptr2 = NULL;
-    void *st = *sptr = NULL;
+    void *st = *sptr;
     int value = 0;
 
     if(ASN__STACK_OVERFLOW_CHECK(opt_codec_ctx))